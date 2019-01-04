6986931b41266a3279bcda90d5b8b88dd38c90d4
SomassekharD <46383899+SomassekharD@users.noreply.github.com>
2019-01-04 13:22:01 -0600

    Audit log file captures the execution time.

---
+++ AuditLogService.java
@@ -0,0 +1,42 @@
+package com.rtct.autoval.service;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@EnableAspectJAutoProxy
+@Component
+public class AuditLogService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuditLogService.class);
+
+	@Pointcut("execution(* com.rtct.autoval.dao..*.*(..))")
+	public void methodPointcut() {
+	}
+
+	@Around("methodPointcut()")
+	public Object profile(final ProceedingJoinPoint pjp) throws Throwable {
+		final StopWatch stopWatch = new StopWatch();
+		try {
+			final long start = getCurrentTime();
+			stopWatch.start();
+			final Object output = pjp.proceed();
+			final long elapsedTime = getCurrentTime() - start;
+			LOGGER.info(pjp.getSignature().getName() + " Execution Time is " + elapsedTime + " ms");
+			return output;
+		} finally {
+			stopWatch.stop();
+		}
+	}
+
+	private long getCurrentTime() {
+		return System.currentTimeMillis();
+	}
+}
